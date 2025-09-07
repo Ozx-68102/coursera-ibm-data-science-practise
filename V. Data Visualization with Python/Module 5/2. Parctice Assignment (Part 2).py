b74a1efe7df81666528c34e92dba2474ce3559e0
Ozx-68102 <ozx1072960726@gmail.com>
2025-09-07 17:08:56 +0800

    [Dataviz with Python]style: Adjust code format

--- V. Data Visualization with Python/Module 5/2. Parctice Assignment (Part 2).py
+++ V. Data Visualization with Python/Module 5/2. Parctice Assignment (Part 2).py
@@ -48,7 +48,7 @@ df = pd.read_csv(filename)
 # Extract year and month from the date column
 datetime = pd.to_datetime(df.loc[:, "Date"]).dt
 df.loc[:, "Year"] = datetime.year
-df.loc[:, "Month"] = datetime.month_name() # used for the names of the months
+df.loc[:, "Month"] = datetime.month_name()  # used for the names of the months
 
 # Create App
 app = Dash(__name__)
@@ -74,16 +74,16 @@ app.layout = html.Div(children=[
                 {"label": "Northern Territory", "value": "NT"},
                 {"label": "Queensland", "value": "QL"},
                 {"label": "South Australia", "value": "SA"},
-                {"label": "Tasmania","value": "TA"},
-                {"label": "Victoria","value": "VI"},
-                {"label": "Western Australia","value": "WA"}
-            ],"NSW", id="region",inline=True)
+                {"label": "Tasmania", "value": "TA"},
+                {"label": "Victoria", "value": "VI"},
+                {"label": "Western Australia", "value": "WA"}
+            ], "NSW", id="region", inline=True)
         ]),
 
-        #Dropdown to select year
+        # Dropdown to select year
         html.Div([
             html.H2("Select Year:", style={"margin-right": "2em"}),
-            dcc.Dropdown(df.Year.unique(), value = 2005,id="year")
+            dcc.Dropdown(df.Year.unique(), value=2005, id="year")
         ]),
 
         # Task 3: Add two empty divisions for output inside the next inner division.
@@ -94,6 +94,7 @@ app.layout = html.Div(children=[
         ], style={"display": "flex"})
     ])
 ])
+
 
 # Task 4: Add the Output and input components inside the app.callback decorator.
 # Place to add @app.callback Decorator
@@ -107,7 +108,6 @@ app.layout = html.Div(children=[
         Input(component_id="year", component_property="value")
     ]
 )
-
 # Task 5: Add the callback function.
 # Place to define the callback function.
 def reg_year_display(region: str, year: int) -> list[dcc.Graph]:
@@ -117,11 +117,13 @@ def reg_year_display(region: str, year: int) -> list[dcc.Graph]:
 
     # Plot 1 - Monthly Average Estimated Fire Area
     est_data = year_data.groupby("Month")["Estimated_fire_area"].mean().reset_index()
-    fig1 = px.pie(est_data, values="Estimated_fire_area", names="Month", title=f"{region} : Monthly Average Estimated Fire Area in year {year}")
+    fig1 = px.pie(est_data, values="Estimated_fire_area", names="Month",
+                  title=f"{region} : Monthly Average Estimated Fire Area in year {year}")
 
     # Plot 2 - Monthly Average Count of Pixels for Presumed Vegetation Fires
     veg_data = year_data.groupby("Month")["Count"].mean().reset_index()
-    fig2 = px.bar(veg_data, x="Month", y="Count", title=f"{region} : Average Count of Pixels for Presumed Vegetation Fires in year {year}")
+    fig2 = px.bar(veg_data, x="Month", y="Count",
+                  title=f"{region} : Average Count of Pixels for Presumed Vegetation Fires in year {year}")
 
     return [dcc.Graph(figure=fig1), dcc.Graph(figure=fig2)]
 
